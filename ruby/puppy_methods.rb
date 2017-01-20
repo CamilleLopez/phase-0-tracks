4c2a83ec1d0a1959b011a063c0c1ab62a97b742e
CamilleLopez <acdacquel@yahoo.com>
2017-01-20 15:07:59 -0800

    add release 0 and 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,6 +5,40 @@ class Puppy
     toy
   end
 
+  def speak
+  puts "how many times"
+  number = gets.chomp.to_i
+  number.times do |x|
+    puts "woof"
+  end 
+  end
+
+
+  def rollover
+  puts "rollover"
+  end
+
+  def age
+  puts "how old are you"
+  age = gets.chomp.to_i * 7
+  p "puppy is #{age} years old"
+  end 
+
+  def jump
+  	puts "Look at me jump"
+  end 
+
+  def initialize
+  	puts "initializing new Puppy instance"
+ end 
+
 end
 
+
+
 ben = Puppy.new
+ben.speak
+ben.fetch("apples")
+ben.rollover
+ben.age
+ben.jump
