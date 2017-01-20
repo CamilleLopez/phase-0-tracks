410e2bb554a3c7821413c12324ee7f8fa117f20e
CamilleLopez <acdacquel@yahoo.com>
2017-01-20 15:42:30 -0800

    add release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -35,10 +35,59 @@ class Puppy
 end
 
 
-
+=begin
 ben = Puppy.new
 ben.speak
 ben.fetch("apples")
 ben.rollover
 ben.age
 ben.jump
+=end
+
+class Humans
+
+	def initialize
+	puts "initializing new Human instance"
+	end 
+
+	def cough
+	puts "cough cough"
+	end 
+
+	def age
+	puts "how old are you"
+	age = gets.chomp.to_i
+	puts "I am #{age}"
+    end
+
+
+
+
+end 
+
+Arra = Humans.new
+Arra.cough
+Arra.age
+
+array = []
+
+50.times do |x|
+	array[x]= Humans.new
+end 
+
+p array
+
+array.each do |x|
+	x.cough
+end 
+
+array.each do |x|
+	x.age
+end
+
+
+
+
+
+
+
