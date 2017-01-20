51d73f2a411df1170cffd70bcd368bb763b4b998
CamilleLopez <acdacquel@yahoo.com>
2017-01-20 14:30:16 -0800

    add puppy species description

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Friendly: varies
+length: varies
+Name: varies
+Fur_Color: varies
+
+BEHAVIOR -----------------------
+fetch
+sit
+drink
+eat
+
+
+
