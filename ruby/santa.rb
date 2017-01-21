f72d7f5927ce8c75b663163e73a3b49cba0bd562
CamilleLopez <acdacquel@yahoo.com>
2017-01-20 16:18:20 -0800

    add eat and speak instance method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,21 @@
 class Santa
   
+  def speak
+  	puts "Ho, ho, ho! Haaaappy holidays!"
+  end 
 
+  def eat_milk_and_cookies(cookie)
+    puts "that was a good #{cookie} cookie"
+  end 
+  
+  def initialize
+  	puts "initializing Santa instance"
+  end 
 
 
 
 
 end 
+randy = Santa.new
+randy.speak
+randy.eat_milk_and_cookies("sugar")
