a9ed56547598a55d33bdc375a9b28d67064b72bd
CamilleLopez <acdacquel@yahoo.com>
2017-02-13 07:36:33 -0800

    add 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,20 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+
+get '/contact' do 
+  "101 SF street"
+end 
+
+get '/great_job' do 
+  names = params[:names]
+  if names
+  "Good job, #{names}!"
+else
+  "Good Job!"
+end
+end 
+
+
