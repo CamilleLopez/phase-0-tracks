e664f3e0cf951c8593ee00188268f282c7ea077a
CamilleLopez <acdacquel@yahoo.com>
2017-02-13 07:26:43 -0800

    9.5

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -6,7 +6,7 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
-
+  
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -16,12 +16,10 @@ end
 get '/students/new' do
   erb :new_student
 end
+#attenance
+get '/students' do
+  @students = db.execute("SELECT * FROM students")
+  erb :names
+end 
 
-# create new students via
-# a form
-post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
-  redirect '/'
-end
 
-# add static resources
